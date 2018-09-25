b0a71c16baccef879538348de588553e95308ccb
AlbertOcampo712 <betoocampo712@gmail.com>
2018-09-25 15:29:36 -0400

    login
    implementando login y sidebar

--- PlataformaEducativa/src/app/app.module.ts
+++ PlataformaEducativa/src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { EstudiantesComponent } from './usuarios/estudiantes/estudiantes.component';
+import { DocentesComponent } from './usuarios/docentes/docentes.component';
+import { AdminComponent } from './usuarios/admin/admin.component';
+import { MenuSidebarComponent } from './menu-sidebar/menu-sidebar.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { ProgramacionComponent } from './semestres/primer/programacion/programacion.component';
+import { LoginComponent } from './login/login.component';
 
+const routes: Routes = [
+  { path: 'login', component:  LoginComponent},
+  { path: 'menusidebar', component:  MenuSidebarComponent},
+  { path: 'pagina-principal', component:  PaginaPrincipalComponent},
+  { path: 'estudiantes', component:  EstudiantesComponent},
+  { path: 'docentes', component:  DocentesComponent},
+  { path: 'admin', component:  AdminComponent},
+  { path: 'programacion', component:  ProgramacionComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstudiantesComponent,
+    DocentesComponent,
+    AdminComponent,
+    MenuSidebarComponent,
+    PaginaPrincipalComponent,
+    ProgramacionComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ PlataformaEducativa/src/app/menu-sidebar/menu-sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
+@Component({
+  selector: 'app-menu-sidebar',
+  templateUrl: './menu-sidebar.component.html',
+  styleUrls: ['./menu-sidebar.component.css']
+})
+export class MenuSidebarComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit( ) {
+  	$(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar, #content').toggleClass('active');
+                $('.collapse.in').toggleClass('in');
+                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+            });
+        });
+  }
+
+}
